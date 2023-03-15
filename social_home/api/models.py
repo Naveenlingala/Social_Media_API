4bfea594dfcdfe34e7dd66908fb8322576c3bd54
naveenlingala <naveenlingala1212@gmail.com>
2023-03-16 04:13:12 +0530

    Defined_Schema

--- social_home/api/models.py
+++ social_home/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followers')
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='following')
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(max_length=3000, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField(max_length=408, blank=False)
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
